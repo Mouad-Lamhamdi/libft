f55b1e47cb9ad9218c3f5164d2c6106657ceeeb7
Mouad-Lamhamdi <lamhamdimouad2@gmail.com>
2024-11-13 14:25:12 +0100

    ll

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molamham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 14:02:55 by molamham          #+#    #+#             */
+/*   Updated: 2024/11/13 14:24:13 by molamham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *current;
+	
+	current = *lst;
+	while (current)
+	{
+		del(current->content);
+		current = current->next;
+	}
+	free(lst);
+}
